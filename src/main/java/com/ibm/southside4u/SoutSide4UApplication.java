623e3f6b997456fa52ddcd4c1d8c187e9735335b
saket <sakkashy@in.ibm.com>
2018-08-17 10:54:50 +0530

    updated to contact eureka server

--- src/main/java/com/ibm/southside4u/SoutSide4UApplication.java
+++ src/main/java/com/ibm/southside4u/SoutSide4UApplication.java
@@ -2,8 +2,10 @@ package com.ibm.southside4u;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SoutSide4UApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/resources/db/changelog/changes/v0003.sql
@@ -0,0 +1,3 @@
+Insert into public.product (producttype,productname) values ('Divya','Divya_1');
+Insert into public.product (producttype,productname) values ('Anitha','Anitha_1');
+Insert into public.product (producttype,productname) values ('Mani','Mani_1');
